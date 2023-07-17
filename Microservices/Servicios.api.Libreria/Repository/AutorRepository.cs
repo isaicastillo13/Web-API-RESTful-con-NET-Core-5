f3ce9cd2f97d93f37727284cd6573d7dbe2f0231
Isaias Castillo <isaicastillo@smrey.com>
2023-07-17 12:18:44 -0500

    Proyecto Ejecuta, pero no trae valores de la base de datos

---
+++ Microservices/Servicios.api.Libreria/Repository/AutorRepository.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using Servicios.api.Libreria.Core.ContextMongoDB;
+using Servicios.api.Libreria.Core.Entities;
+using Servicios.api.Libreria.Repository;
+
+namespace Servicios.api.libreria.Repository
+{
+    public class AutorRepository : IAutorRepository
+    {
+        private readonly IAutorContext _autorContext;
+        public AutorRepository(IAutorContext autorContext)
+        {
+            _autorContext = autorContext;
+        }
+
+        public async Task<IEnumerable<Autor>> GetAutores()
+        {
+            return await _autorContext.Autores.Find(p => true).ToListAsync();
+        }
+    }
+}
